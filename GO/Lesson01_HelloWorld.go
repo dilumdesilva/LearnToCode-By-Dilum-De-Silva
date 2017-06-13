a842caca49d90e5dead670e3a547d551b3f90e4c
Dilum De Silva <dilumdesilva.edu@gmail.com>
2017-06-13 11:48:21 +0530

    Add files via upload

---
+++ GO/Lesson01_HelloWorld.go
@@ -0,0 +1,13 @@
+//main package
+package main
+
+//importing fmt library
+import "fmt"
+
+//main function
+func main() {
+    
+    //the way of printing something
+    fmt.Println("Hello World")
+    fmt.Println("I am Dilum")
+}
