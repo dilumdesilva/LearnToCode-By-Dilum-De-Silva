2ede45dfe0b9ef2eb010402ced7d499ed736ab68
Dilum De Silva <dilumdesilva.edu@gmail.com>
2017-06-16 06:07:23 +0530

    project added

---
+++ JavaBasics/src/ParameterPassingFromAMethod.java
@@ -0,0 +1,10 @@
+//this class includes the code related to parameter passing and returning values from a method.
+
+public class ParameterPassingFromAMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
