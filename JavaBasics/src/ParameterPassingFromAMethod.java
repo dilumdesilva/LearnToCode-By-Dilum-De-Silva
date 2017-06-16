6739e93f3c627b6c8da8e36bfe3880c460056792
Dilum De Silva <dilumdesilva.edu@gmail.com>
2017-06-16 06:48:39 +0530

    parameter passing lesson commited

--- JavaBasics/src/ParameterPassingFromAMethod.java
+++ JavaBasics/src/ParameterPassingFromAMethod.java
@@ -4,7 +4,13 @@ public class ParameterPassingFromAMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Method 'add' has invoked result of 10 + 15+ 20 = ");
+		add(10,15,20);
+	}
+	
+	public static void add(int a,int b,int c){
+		
+		System.out.println(a+b+c);
 	}
 
 }
